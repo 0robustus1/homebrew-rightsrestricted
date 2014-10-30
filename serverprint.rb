99f37425ece0a5e321af6806e1cbcaf14c6ba7f5
Tim Reddehase <robustus@rightsrestricted.com>
2014-10-30 15:25:39 +0100

    bump serverprint 0.0.5.

--- serverprint.rb
+++ serverprint.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Serverprint < Formula
   homepage "https://github.com/0robustus1/serverprint"
-  url "https://github.com/0robustus1/serverprint/archive/0.0.4.tar.gz"
-  sha1 "14fb9da43f8da643b963fb430546d5bd694217e7"
-  version "0.0.4"
+  url "https://github.com/0robustus1/serverprint/archive/0.0.5.tar.gz"
+  sha1 "844ace272599e94e53e50808c8dfc22ca364eaeb"
+  version "0.0.5"
 
   def install
     prefix.install Dir['*']
