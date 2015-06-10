35db4b6abf0968e4455516bf01127b24f1c8e42b
Tim Reddehase <robustus@rightsrestricted.com>
2015-06-10 15:23:55 +0200

    upgrade serverprint to 0.1.2.

--- serverprint.rb
+++ serverprint.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Serverprint < Formula
   homepage "https://github.com/0robustus1/serverprint"
-  url "https://github.com/0robustus1/serverprint/archive/0.1.1.tar.gz"
-  sha1 "d26352c5007ffb9c9d91b4c79685946b41c60fe7"
-  version "0.1.1"
+  url "https://github.com/0robustus1/serverprint/archive/0.1.2.tar.gz"
+  sha1 "270e6e137b9922ef103d62f5ef62d06d0b4c4420"
+  version "0.1.2"
 
   def install
     system("make prefix=#{prefix} install")
